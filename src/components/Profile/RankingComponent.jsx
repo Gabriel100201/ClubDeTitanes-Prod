5121ba9870e2417dd435f1fcf2334640311b0f37
Gabriel <gabrielfunes100202@gmail.com>
2024-09-15 13:18:15 -0300

    Arreglo ranking de usuarios

--- src/components/Profile/RankingComponent.jsx
+++ src/components/Profile/RankingComponent.jsx
@@ -19,26 +19,26 @@ export const RankingComponment = () => {
     return (
 
         <div className="w-full max-w-4xl">
-            <h1 className="text-center text-white text-2xl font-bold mb-8">Ranking de Usuarios</h1>
+            <h1 className="text-center text-white text-3xl lg:text-4xl font-bold mb-8 px-3">Ranking de Usuarios</h1>
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                            <th scope="col" className="px-6 py-3">Posición</th>
-                            <th scope="col" className="px-6 py-3">Nombre</th>
-                            <th scope="col" className="px-6 py-3">Puntos</th>
+                <table className="w-full text-sm text-left text-gray-500">
+                    <thead className="text-xs text-gray-700 uppercase bg-gradient-to-b from-primary-200 to-primary-500">
+                        <tr className="">
+                            <th scope="col" className="px-3 md:px-6 py-3">Posición</th>
+                            <th scope="col" className="px-3 md:px-6 py-3">Nombre</th>
+                            <th scope="col" className="px-3 md:px-6 py-3">Puntos</th>
                         </tr>
                     </thead>
                     <tbody>
                         {users.map((user, index) => (
-                            <tr key={user.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <tr key={user.id} className="bg-white border-b text-gray-900 whitespace-nowrap font-medium">
+                                <th scope="row" className="px-3 md:px-6 py-4 font-medium">
                                     {index + 1}
                                 </th>
-                                <td className="px-6 py-4">
+                                <td className="px-3 md:px-6 py-4">
                                     {user.username}
                                 </td>
-                                <td className="px-6 py-4">
+                                <td className="px-3 md:px-6 py-4">
                                     {user.points}
                                 </td>
                             </tr>
--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -3,7 +3,7 @@ import { RankingComponment } from "../components/Profile/RankingComponent"
 export const Ranking = () => {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center items-center min-h-[calc(100vh-180px)] py-12 bg-gradient-to-b from-alternative-950 via-alternative-700 to-alternative-950">
         <RankingComponment />
       </div>
     </>
