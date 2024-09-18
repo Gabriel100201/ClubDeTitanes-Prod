d171535db4de85aad53f0754e90302ec4782f45a
Gabriel <gabrielfunes100202@gmail.com>
2024-09-18 14:18:23 -0300

    Añado posibilidad de usuario admin 2.1

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -22,7 +22,8 @@ export const useAuth = () => {
     if (!result.token) {
       throw new Error(result);
     }
-    const userData = { email: email, token: result.token, points: result.points, isProUser: result.userType, username: result.username, code: result.referral_code, isAdmin: result.is_admin };
+    const userData = { email: email, token: result.token, points: result.points, isProUser: result.userType, username: result.username, code: result.referral_code, isAdmin: result.isAdmin };
+    console.log(userData);
     login(userData);
     navigate('/');
   };
