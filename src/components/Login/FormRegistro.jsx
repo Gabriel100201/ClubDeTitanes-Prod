cac9295947b697938bb62869ab64933bf8e35715
Gabriel <gabrielfunes100202@gmail.com>
2024-09-20 17:16:32 -0300

    Cambio nombre por defecto a Usuario

--- src/components/Login/FormRegistro.jsx
+++ src/components/Login/FormRegistro.jsx
@@ -73,7 +73,7 @@ export const FormRegistro = () => {
                                             name="username"
                                             id="username"
                                             className="rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-                                            placeholder="JuanPerez"
+                                            placeholder="Usuario"
                                             required
                                             value={username}
                                             onChange={(e) => setUsername(e.target.value)}
